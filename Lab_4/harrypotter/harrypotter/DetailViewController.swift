a17185f5492876a1d0071e96271b91e8817650f1
CMcGuinness <comc3361@colorado.edu>
2016-02-11 10:54:40 -0700

    lab 4 updated

--- Lab_4/harrypotter/harrypotter/DetailViewController.swift
+++ Lab_4/harrypotter/harrypotter/DetailViewController.swift
@@ -8,11 +8,16 @@
 
 import UIKit
 
-class DetailViewController: UIViewController {
+class DetailViewController: UIViewController, UIWebViewDelegate {
 
     @IBOutlet weak var detailDescriptionLabel: UILabel!
 
-
+    @IBOutlet weak var webView: UIWebView!
+    
+    @IBOutlet weak var webSpinner: UIActivityIndicatorView!
+    
+    
+    // reference to selection from master view
     var detailItem: AnyObject? {
         didSet {
             // Update the view.
@@ -20,13 +25,35 @@ class DetailViewController: UIViewController {
         }
     }
 
+    // updates label with the detail item
     func configureView() {
         // Update the user interface for the detail item.
         if let detail = self.detailItem {
             if let label = self.detailDescriptionLabel {
                 label.text = detail.description
+                loadWebPage(detail.description)
             }
         }
+    }
+    
+    func loadWebPage(urlString: String){
+        
+        //create NSURL object
+        let url = NSURL(string: urlString)
+        
+        //create NSURLRequest object
+        let request = NSURLRequest(URL: url!)
+        
+        //load the NSURLRequest object in our web view
+        webView.loadRequest(request)
+    }
+    
+    func webViewDidStartLoad(webView: UIWebView) {
+        webSpinner.startAnimating()
+    }
+    
+    func webViewDidFinishLoad(webView: UIWebView) {
+        webSpinner.stopAnimating()
     }
 
     override func viewDidLoad() {
--- Lab_4/harrypotter/harrypotter/MasterViewController.swift
+++ Lab_4/harrypotter/harrypotter/MasterViewController.swift
@@ -11,20 +11,31 @@ import UIKit
 class MasterViewController: UITableViewController {
 
     var detailViewController: DetailViewController? = nil
-    var objects = [AnyObject]()
+    var characters = [[String: String]]()
 
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        self.navigationItem.leftBarButtonItem = self.editButtonItem()
+        /*self.navigationItem.leftBarButtonItem = self.editButtonItem()
 
         let addButton = UIBarButtonItem(barButtonSystemItem: .Add, target: self, action: "insertNewObject:")
         self.navigationItem.rightBarButtonItem = addButton
         if let split = self.splitViewController {
             let controllers = split.viewControllers
             self.detailViewController = (controllers[controllers.count-1] as! UINavigationController).topViewController as? DetailViewController
+        
+        */
+        let path = NSBundle.mainBundle().pathForResource("harrypotter", ofType: "plist")
+        
+        let alldata = NSDictionary(contentsOfFile: path!) as! [String:[[String : String]]]
+        
+        if alldata.isEmpty != true {
+            characters = Array(alldata["characters"]!)
         }
+        
+        
+        
     }
 
     override func viewWillAppear(animated: Bool) {
@@ -37,20 +48,23 @@ class MasterViewController: UITableViewController {
         // Dispose of any resources that can be recreated.
     }
 
-    func insertNewObject(sender: AnyObject) {
+    /*func insertNewObject(sender: AnyObject) {
         objects.insert(NSDate(), atIndex: 0)
         let indexPath = NSIndexPath(forRow: 0, inSection: 0)
         self.tableView.insertRowsAtIndexPaths([indexPath], withRowAnimation: .Automatic)
-    }
+    }*/
 
     // MARK: - Segues
 
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         if segue.identifier == "showDetail" {
             if let indexPath = self.tableView.indexPathForSelectedRow {
-                let object = objects[indexPath.row] as! NSDate
+                let character = characters[indexPath.row]
+                let url = character["url"]! as String
+                let name = character["name"]! as String
                 let controller = (segue.destinationViewController as! UINavigationController).topViewController as! DetailViewController
-                controller.detailItem = object
+                controller.detailItem = url
+                controller.title = name
                 controller.navigationItem.leftBarButtonItem = self.splitViewController?.displayModeButtonItem()
                 controller.navigationItem.leftItemsSupplementBackButton = true
             }
@@ -64,30 +78,30 @@ class MasterViewController: UITableViewController {
     }
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return objects.count
+        return characters.count
     }
 
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath)
 
-        let object = objects[indexPath.row] as! NSDate
-        cell.textLabel!.text = object.description
+        let character = characters[indexPath.row]
+        cell.textLabel!.text = character["name"]! as String
         return cell
     }
 
     override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
         // Return false if you do not want the specified item to be editable.
-        return true
+        return false
     }
 
-    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+    /*override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
         if editingStyle == .Delete {
             objects.removeAtIndex(indexPath.row)
             tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
         } else if editingStyle == .Insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view.
         }
-    }
+    }*/
 
 
 }
