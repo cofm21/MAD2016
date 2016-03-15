1e6d5fdc26694767a3632e750ae82fb52bc4e1ee
CMcGuinness <comc3361@colorado.edu>
2016-03-15 09:42:25 -0600

    added lab 7

--- Midterm/groceryList/groceryList/DetailViewController.swift
+++ Midterm/groceryList/groceryList/DetailViewController.swift
@@ -82,15 +82,13 @@ class DetailViewController: UITableViewController {
     
     // Override to support editing the table view.
     override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        if editingStyle == .Delete {
+        
             // Delete the row from the data source
             let chosenStore = storeListDetail.store[selectedStore]
             storeListDetail.storeData[chosenStore]?.removeAtIndex(indexPath.row)
             
             tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+   
     }
     
 
